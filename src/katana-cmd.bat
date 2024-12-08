cf4ca4560480fadfe18877aba8a902ed47afa393
Katana. <97043303+universestate@users.noreply.github.com>
2024-12-08 10:58:01 +0700

    Add files via upload

---
+++ src/katana-cmd.bat
@@ -0,0 +1,273 @@
+:: Katana Tools SA-MP - Commands.
+:: Copyright (c) 2024 Katana® All rights reserved.
+:: MIT License:
+:: Copyright 2024 Katana®
+:: Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+:: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+:: THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+:: This is a project from my hobby,
+
+@echo off
+
+:: CMYK color.
+color c
+
+:: title for label Caption Terminal.
+title %date%
+
+:: Delayed Variable Expansion.
+setlocal enabledelayedexpansion
+
+:: set variable.
+set "SystemExDir=%~dp0" :: current dir
+set "version=2024.latest.Now ^(0.0.3^)" :: current version.
+
+:: command line
+:cmd
+echo **************************
+echo.
+echo $ command:
+echo ** [General]
+echo * Katana     :       katana        : What is Katana?
+echo * Katana     :       build         : build Katana compiler.
+echo * Katana     :       start         : Start Katana compiler.
+echo * Katana     :       running       : running server's.
+echo * Katana     :       start-run     : compile + run server..
+echo * Katana     :       build-start   : build + compile..
+echo.
+echo ** [Random]
+echo * Katana     :       version       : Check Your Katana Version.
+echo * Katana     :       profile       : Check Your Windows User Profile.
+echo * Katana     :       username      : Check Your Windows Username.
+echo * Katana     :       time          : Check Your Current Time.
+echo * Katana     :       date          : Check Your Current Date.
+echo.
+echo ** [Startup]
+echo * Katana     :       credit        : Katana Credit.
+echo * Katana     :       tasks         : Create VSCode Task Katana Compiler.
+echo * Katana     :       example       : Create Example Katana Funcc SA-MP.
+echo * Katana     :       clear         : Clear Terminal Screen, Back to main menu.
+echo.
+echo **************************
+
+echo.
+set /p command="** input cmd: > $ "
+
+:: response for command.
+if "%command%"=="katana" (
+
+echo msgbox "Katana SA-MP is a compilation tool with multiple functions for the Pawn Compiler in SA-MP." > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+
+echo msgbox "You do not need to use symbols like ';' and '( )' and other in your program code if using this software. For example, you can use 'example' and immediately check the guidelines\helloworld.pwn file for more information." > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+
+echo msgbox "-[WARNING]-: If you use the code structure from Katana, you are expected to compile it using the software provided by katana. Using the default Pawn Editor software will cause errors." > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+
+echo msgbox "Thank you for using this software <:" > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+
+) else if "%command%"=="build" (
+
+    echo :: Building compiler...
+    start "" "katana-setup.bat"
+
+) else if "%command%"=="start" (
+
+    echo :: Compiling...
+    start "" "katana-start.bat"
+
+) else if "%command%"=="running" (
+    start "" samp-server.exe
+
+    timeout /t 0 /nobreak >nul
+
+    if %ERRORLEVEL% neq 0 (
+        echo :: The program failed to run. Checking logs...Program failed to run. Checking logs...
+        
+        if exist "server_log.txt" (
+            echo :: Opening server_log.txt...
+            start "" "notepad" "server_log.txt"
+        ) else (
+            echo :: server_log.txt not found.
+        )
+    ) else (
+        echo :: The program was executed successfully.
+    )
+
+) else if "%command%"=="start-run" (
+
+    echo :: Compiling..
+    start "" "katana-start.bat"
+    
+    echo Press any key to Start Your Server's . . .
+    pause >nul
+
+    start "" samp-server.exe
+
+    timeout /t 0 /nobreak >nul
+
+    if %ERRORLEVEL% neq 0 (
+        echo :: The program failed to run. Checking logs...Program failed to run. Checking logs...
+        
+        if exist "server_log.txt" (
+            echo :: Opening server_log.txt...
+            start "" "notepad" "server_log.txt"
+        ) else (
+            echo :: server_log.txt not found.
+        )
+    ) else (
+        echo :: The program was executed successfully.
+    )
+
+) else if "%command%"=="build-start" (
+    start "" "katana-setup.bat"
+
+    echo Press any key to open Compiler . . .
+    pause >nul
+
+    start "" "katana-start.bat"
+) else if "%command%"=="version" (
+    echo.
+    echo %version%
+    echo.
+
+    echo msgbox "Your Katana Version: 2024.latest.Now - build (0.0.3)" > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+) else if "%command%"=="time" (
+    echo.
+    echo %time%
+    echo.
+) else if "%command%"=="date" (
+    echo.
+    echo %date%
+    echo.
+) else if "%command%"=="tasks" (
+    if not exist ".vscode" (
+        mkdir ".vscode"
+    ) else ( 
+        echo.
+        echo :: the task already exists!..
+        echo.
+        goto end
+    )
+
+    echo.
+    echo :: Creating .vscode\tasks.json...
+
+    timeout /t 1
+
+    echo {>> ".vscode\tasks.json"
+    echo   "version": "2.0.0",>> ".vscode\tasks.json"
+    echo   "tasks": [>> ".vscode\tasks.json"
+    echo     {>> ".vscode\tasks.json"
+    echo       "label": "Run Katana SA-MP CMD",>> ".vscode\tasks.json"
+    echo       "type": "shell",>> ".vscode\tasks.json"
+    echo       "command": "start",>> ".vscode\tasks.json"
+    echo       "args": [>> ".vscode\tasks.json"
+    echo           "${workspaceRoot}/katana-start.bat">> ".vscode\tasks.json"
+    echo       ],>> ".vscode\tasks.json"
+    echo       "group": {>> ".vscode\tasks.json"
+    echo           "kind": "build",>> ".vscode\tasks.json"
+    echo           "isDefault": true>> ".vscode\tasks.json"
+    echo       },>> ".vscode\tasks.json"
+    echo       "problemMatcher": [],>> ".vscode\tasks.json"
+    echo       "presentation": {>> ".vscode\tasks.json"
+    echo           "reveal": "silent">> ".vscode\tasks.json"
+    echo       }>> ".vscode\tasks.json"
+    echo     }>> ".vscode\tasks.json"
+    echo   ]>> ".vscode\tasks.json"
+    echo }>> ".vscode\tasks.json"
+
+    echo.
+    echo :: Task configuration created successfully in .vscode\tasks.json.
+    echo.
+
+    goto end
+) else if "%command%"=="clear" (
+    cls
+    goto cmd
+) else if "%command%"=="example" (
+    if not exist "guidelines" (
+        mkdir "guidelines"
+    ) else (
+        echo.
+        echo :: the guide already exists!
+        echo.
+        goto end
+    )
+
+    echo.
+
+    echo :: Creating guidelines\helloworld.pwn...
+
+    timeout /t 1
+    
+    echo /* example */>> "guidelines\helloworld.pwn"
+
+    echo #include "a_samp">> "guidelines\helloworld.pwn"
+    echo.>> "guidelines\helloworld.pwn"
+    
+    echo #pragma tabsize 4 >> "guidelines\helloworld.pwn"
+    echo #pragma warning disable 217 >> "guidelines\helloworld.pwn"
+    echo.>> "guidelines\helloworld.pwn"
+
+    echo ^main(^) {>> "guidelines\helloworld.pwn"
+    echo printthis:>> "guidelines\helloworld.pwn"
+    echo    print "Hello, World!">> "guidelines\helloworld.pwn"
+    echo        goto printthis;>> "guidelines\helloworld.pwn"
+    echo    return 1;>> "guidelines\helloworld.pwn"
+    echo }>> "guidelines\helloworld.pwn"
+
+    echo.>> "guidelines\helloworld.pwn"
+
+    echo ^public OnPlayerConnect(playerid^) {>> "guidelines\helloworld.pwn"
+        echo    SendClientMessage playerid, -1, "Hello">> "guidelines\helloworld.pwn"
+        echo    return 1;>> "guidelines\helloworld.pwn"
+    echo }>> "guidelines\helloworld.pwn"
+
+    echo.
+    echo :: Example tool created in guidelines\helloworld.pwn.
+    echo.
+    
+    goto end
+)  else if "%command%"=="credit" (
+    echo msgbox "Katana Credit: anonymous (S)" > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+
+    echo github ^(S*^) : https://github.com/universestate/
+) else if "%command%"=="profile" (
+    echo.
+    echo :: Profile: %userprofile%
+    echo.
+) else if "%command%"=="username" (
+    echo.
+    echo :: Current user: %username%
+    echo.
+) else if "%command%"=="hello" (
+    echo msgbox "Hello, Man :>" > "%tmp%\tmp.vbs"
+    cscript /nologo "%tmp%\tmp.vbs"
+    del "%tmp%\tmp.vbs"
+) else if "%command%"=="" (
+    goto cmd
+) else (
+    echo.
+    echo :: $ '%command%' : This command does not exist. Please try again..
+
+    timeout /t 1
+    goto cmd
+)
+
+:end
+echo :: Press any key to return . . .
+pause >nul
+
+goto cmd
