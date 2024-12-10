e2699a31c185b36a32ac024cb8701388b654af2b
UniverseState <97043303+universestate@users.noreply.github.com>
2024-12-10 15:20:30 +0700

    Update pawn-start.cmd

--- src/batch/pawn-start.cmd
+++ src/batch/pawn-start.cmd
@@ -1,4 +1,3 @@
-:: @start
 @echo off
 
 :: CMYK color.
@@ -20,11 +19,11 @@ set "katanaFile=%katanaDir%system.ini"
 
 :next
 if not exist "%katanaFile%" (
-echo :: system.ini is required to determine the gamemode..
+    echo :: system.ini is required to determine the gamemode..
     start "" "pawn-setup.cmd"
-        echo.
-            echo ; Go out...
-        timeout /t 5
+    echo.
+    echo ; Go out...
+    timeout /t 5
     exit
 )
 
@@ -40,7 +39,7 @@ for /f "tokens=1,2 delims==" %%a in ('findstr /c:"target=" "%katanaFile%"') do (
 
 if not defined katana_path_file (
     echo :: system.ini is missing gamemode information.
-        timeout /t 5
+    timeout /t 5
     exit
 )
 
@@ -55,7 +54,7 @@ for /f "tokens=1,2 delims==" %%a in ('findstr /c:"drive=" "%katanaFile%"') do (
 
 if not defined katana_path_gm (
     echo :: drive not found in system.ini.
-        timeout /t 5
+    timeout /t 5
     exit
 )
 
@@ -65,7 +64,7 @@ set "katana_path_gm=%katanaDir%!katana_path_gm!"
 
 if not exist "!katana_path_gm!" (
     echo :: Gamemodes folder not found: !katana_path_gm!.
-        timeout /t 5
+    timeout /t 5
     exit
 )
 
@@ -80,26 +79,31 @@ for /r "%katanaDir%" %%p in (pawncc.exe) do (
 :found_pawncc
 if not defined katana_pawncc_path (
     echo :: pawncc.exe not found in any subdirectories.
-        pause
+    pause
     exit /b
 )
 
 if exist "!katana_path_gm!\!katana_path_file!.pwn" ( set "file_extension=.pwn" ) else if exist "!katana_path_gm!\!katana_path_file!.kn" ( set "file_extension=.kn" ) else if exist "!katana_path_gm!\!katana_path_file!.p" ( set "file_extension=.p" ) else (
-    echo :: [ERROR]: No '.pwn' or '.p' or '.kn' =^> !katana_path_file! in drive "!katana_path_gm!"
-        pause
+    echo :: [ERROR]: No '.pwn' or '.p' or '.kn' found for !katana_path_file! in drive "!katana_path_gm!"
+    pause
     exit /b
 )
 
 echo Found file: !katana_path_file!!file_extension!
-    echo Starting compilation...
+echo Starting compilation...
 
-"!katana_pawncc_path!" "!katana_path_gm!\!katana_path_file!!file_extension!" -o"!katana_path_gm!\!katana_path_file!.amx" :: docs, please see: https://github.com/universestate/katana/tree/main/docs#:~:text=2%20hours%20ago-,README.md,-Update%20README.md
+:: Compile the Pawn file using pawncc.exe
+"!katana_pawncc_path!" "!katana_path_gm!\!katana_path_file!!file_extension!" -o"!katana_path_gm!\!katana_path_file!.amx"
 
 if exist "!katana_path_gm!\!katana_path_file!.amx" (
-echo Compilation successful: !katana_path_file!.amx created in the folder.
-echo.
-    for %%A in ("!katana_path_gm!\!katana_path_file!.amx") do ( echo :: Size of !katana_path_file!.amx: %%~zA bytes )
-) else ( echo :: ^(failed^): Compilation failed for !katana_path_file!.. )
+    echo Compilation successful: !katana_path_file!.amx created in the folder.
+    echo.
+    for %%A in ("!katana_path_gm!\!katana_path_file!.amx") do (
+        echo :: Size of !katana_path_file!.amx: %%~zA bytes
+    )
+) else (
+    echo :: ^(failed^): Compilation failed for !katana_path_file!..
+)
 echo.
 
 echo :: Press any key to return . . .
