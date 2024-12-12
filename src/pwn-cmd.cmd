433897c56823f4736ed73e74448e9c09c1d52600
UniverseState <97043303+universestate@users.noreply.github.com>
2024-12-12 18:07:40 +0700

    Update pwn-cmd.cmd

--- src/pwn-cmd.cmd
+++ src/pwn-cmd.cmd
@@ -45,6 +45,7 @@ set /p command="[%mytime%]/Enter Command:~$ "
 
 if "%command%"=="0" (
 
+:katanas
 echo msgbox "Katana is a compilation tool with multiple functions for the Pawn Compiler in SA-MP." > "%tmp%\tmp.vbs"
     cscript /nologo "%tmp%\tmp.vbs"
         del "%tmp%\tmp.vbs"
@@ -56,21 +57,25 @@ echo msgbox "-[WARNING]-: If you use the code structure from Katana, you are exp
 echo msgbox "Thank you for using this software :)" > "%tmp%\tmp.vbs"
     cscript /nologo "%tmp%\tmp.vbs"
         del "%tmp%\tmp.vbs"
+goto end
 
 ) else if "%command%"=="1" (
 
+:builds
 echo [System]/Building compiler...
     start "" "pwn-build.cmd"
     goto cmd
 
 ) else if "%command%"=="2" (
 
+:starts
 echo [System]/Compiling...
     start "" "pwn-start.cmd"
     goto cmd
 
 ) else if "%command%"=="3" (
 
+:bstarts
     start "" "pwn-build.cmd"
 
     echo Press any key to open Compiler . . .
@@ -80,6 +85,7 @@ echo [System]/Compiling...
 
 ) else if "%command%"=="4" (
 
+:runns
     start "" "samp-server.exe"
 
     timeout /t 1 >nul
@@ -100,7 +106,8 @@ echo [System]/Compiling...
     )
 
 ) else if "%command%"=="5" (
-    
+
+:sruuns
 echo [System]/Compiling..
     start "" "pwn-start.cmd"
     
@@ -128,6 +135,7 @@ echo [System]/Compiling..
 
 ) else if "%command%"=="6" (
 
+:versions
     echo.
         echo Current Katana Version =^> %version%
     echo.
@@ -137,13 +145,15 @@ echo [System]/Compiling..
             del "%tmp%\tmp.vbs"
 
 ) else if "%command%"=="7" (
-    
+
+:usernames
     echo.
         echo Current Username =^> %username%
             echo.
 
 ) else if "%command%"=="8" (
-    
+
+:taskss
     if not exist ".vscode" (
         mkdir ".vscode"
     ) else ( 
@@ -187,7 +197,8 @@ echo [System]/Compiling..
     goto end
 
 ) else if "%command%"=="9" (
-    
+
+:guides
     if not exist "guidelines" (
         mkdir "guidelines"
     ) else (
@@ -230,205 +241,55 @@ echo [System]/Compiling..
     goto end
 
 ) else if "%command%"=="10" (
-    
+
+:clears
     cls
     goto ascii
     goto cmd
 
 ) else if "%command%"=="-katana" (
 
-echo msgbox "Katana is a compilation tool with multiple functions for the Pawn Compiler in SA-MP." > "%tmp%\tmp.vbs"
-    cscript /nologo "%tmp%\tmp.vbs"
-        del "%tmp%\tmp.vbs"
-
-echo msgbox "You do not need to use symbols like ';' and '( )' and other in your program code if using this software. For example, you can use '-example' and immediately check the guidelines\helloworld.pwn file for more information." > "%tmp%\tmp.vbs"
-    cscript /nologo "%tmp%\tmp.vbs"
-        del "%tmp%\tmp.vbs"
-
-echo msgbox "-[WARNING]-: If you use the code structure from Katana, you are expected to compile it using the software provided by katana. Using the default Pawn Editor software will cause errors." > "%tmp%\tmp.vbs"
-    cscript /nologo "%tmp%\tmp.vbs"
-        del "%tmp%\tmp.vbs"
-
-echo msgbox "Thank you for using this software :)" > "%tmp%\tmp.vbs"
-    cscript /nologo "%tmp%\tmp.vbs"
-        del "%tmp%\tmp.vbs"
+goto katanas
 
 ) else if "%command%"=="-build" (
 
-echo [System]/Building compiler...
-    start "" "pwn-build.cmd"
-    goto cmd
+goto builds
 
 ) else if "%command%"=="-start" (
-    
-echo [System]/Compiling...
-    start "" "pwn-start.cmd"
-    goto cmd
+
+goto starts
 
 ) else if "%command%"=="-bstart" (
 
-    start "" "pwn-build.cmd"
-
-    echo Press any key to open Compiler . . .
-        pause >nul
-            start "" "pwn-start.cmd"
+goto bstarts
 
 ) else if "%command%"=="-runn" (
 
-    start "" "samp-server.exe"
-
-    timeout /t 2 >nul
-
-    tasklist | find /i "samp-server.exe" >nul
-
-    if errorlevel 1 (
-        echo [System]/The program failed to run. Checking logs...Program failed to run. Checking logs...
-        
-        if exist "server_log.txt" (
-            echo [System]/Opening server_log.txt...
-            start "" "notepad" "server_log.txt"
-        ) else (
-            echo [System]/server_log.txt not found.
-        )
-    ) else (
-        echo [System]/The program was executed successfully.
-    )
+goto ruuns
 
 ) else if "%command%"=="-srunn" (
 
-echo [System]/Compiling..
-    start "" "pwn-start.cmd"
-    
-    echo Press any key to Start Your Server's . . .
-        pause >nul
-
-    start "" "samp-server.exe"
-
-    timeout /t 2 >nul
-
-    tasklist | find /i "samp-server.exe" >nul
-
-    if errorlevel 1 (
-        echo [System]/The program failed to run. Checking logs...Program failed to run. Checking logs...
-        
-        if exist "server_log.txt" (
-            echo [System]/Opening server_log.txt...
-            start "" "notepad" "server_log.txt"
-        ) else (
-            echo [System]/server_log.txt not found.
-        )
-    ) else (
-        echo [System]/The program was executed successfully.
-    )
+goto sruuns
 
 ) else if "%command%"=="-version" (
 
-    echo.
-        echo Current Katana Version =^> %version%
-    echo.
-
-    echo msgbox "Your Katana Version: 2024.latest.Now - build (0.0.4)" > "%tmp%\tmp.vbs"
-        cscript /nologo "%tmp%\tmp.vbs"
-            del "%tmp%\tmp.vbs"
+goto versions
 
 ) else if "%command%"=="-username" (
 
-    echo.
-        echo Current Username =^> %username%
-            echo.
+goto usernames
 
 ) else if "%command%"=="-tasks" (
 
-    if not exist ".vscode" (
-        mkdir ".vscode"
-    ) else ( 
-        echo.
-            echo [System]/the task already exists!..
-                echo.
-        goto end
-    )
-
-    echo.
-        echo [System]/Creating .vscode\tasks.json...
-
-    timeout /t 1
-
-    echo {>> ".vscode\tasks.json"
-        echo   "version": "2.0.0",>> ".vscode\tasks.json"
-        echo   "tasks": [>> ".vscode\tasks.json"
-        echo     {>> ".vscode\tasks.json"
-        echo       "label": "Run Katana SA-MP CMD",>> ".vscode\tasks.json"
-        echo       "type": "shell",>> ".vscode\tasks.json"
-        echo       "command": "start",>> ".vscode\tasks.json"
-        echo       "args": [>> ".vscode\tasks.json"
-        echo           "${workspaceRoot}/pwn-start.cmd">> ".vscode\tasks.json"
-        echo       ],>> ".vscode\tasks.json"
-        echo       "group": {>> ".vscode\tasks.json"
-        echo           "kind": "build",>> ".vscode\tasks.json"
-        echo           "isDefault": true>> ".vscode\tasks.json"
-        echo       },>> ".vscode\tasks.json"
-        echo       "problemMatcher": [],>> ".vscode\tasks.json"
-        echo       "presentation": {>> ".vscode\tasks.json"
-        echo           "reveal": "silent">> ".vscode\tasks.json"
-        echo       }>> ".vscode\tasks.json"
-        echo     }>> ".vscode\tasks.json"
-        echo   ]>> ".vscode\tasks.json"
-    echo }>> ".vscode\tasks.json"
-
-    echo.
-        echo [System]/Task configuration created successfully in .vscode\tasks.json.
-            echo.
-
-    goto end
+goto taskss
 
 ) else if "%command%"=="-example" (
 
-    if not exist "guidelines" (
-        mkdir "guidelines"
-    ) else (
-        echo.
-            echo [System]/the example already exists!
-                echo.
-        goto end
-    )
-
-    echo.
-        echo [System]/Creating guidelines\helloworld.pwn...
-
-    timeout /t 1
-    
-    echo /* example */>> "guidelines\helloworld.pwn"
-
-        echo #include "a_samp">> "guidelines\helloworld.pwn"
-        echo.>> "guidelines\helloworld.pwn"
-        
-        echo #pragma tabsize 4 >> "guidelines\helloworld.pwn"
-        echo.>> "guidelines\helloworld.pwn"
-
-        echo ^main(^) {>> "guidelines\helloworld.pwn"
-        echo printthis:>> "guidelines\helloworld.pwn"
-        echo    print "Hello, World!">> "guidelines\helloworld.pwn"
-        echo        goto printthis;>> "guidelines\helloworld.pwn"
-        echo }>> "guidelines\helloworld.pwn"
-
-        echo.>> "guidelines\helloworld.pwn"
-
-        echo ^public OnPlayerConnect(playerid^) {>> "guidelines\helloworld.pwn"
-            echo    SendClientMessage playerid, -1, "Hello">> "guidelines\helloworld.pwn"
-            echo    return 1;>> "guidelines\helloworld.pwn"
-        echo }>> "guidelines\helloworld.pwn"
-
-    echo.
-        echo [System]/Example tool created in guidelines\helloworld.pwn.
-            echo.
-    
-    goto end
+goto guides
 
 ) else if "%command%"=="-clear" (
 
-    cls
-    goto ascii
-    goto cmd
+goto clears
 
 ) else if "%command%"=="help" (
 
