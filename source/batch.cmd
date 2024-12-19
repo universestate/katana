6ffb754cdfaafbec8e5dcbcf149144c099b61180
Socket <97043303+universestate@users.noreply.github.com>
2024-12-19 13:46:56 +0700

    Update batch.cmd

--- source/batch.cmd
+++ source/batch.cmd
@@ -294,7 +294,7 @@ goto :eof
     echo Starting compilation..
     echo.
 
-    "!laterium_pawncc_path!" "!laterium_path_gm!\!laterium_path_file!!file_extension!" -o"!laterium_path_gm!\!laterium_path_file!.amx" -d0 > rus.txt ::
+    "!laterium_pawncc_path!" "!laterium_path_gm!\!laterium_path_file!!file_extension!" -o"!laterium_path_gm!\!laterium_path_file!.amx" -d0 > rus.txt
 
     if exist "!laterium_path_gm!\!laterium_path_file!.amx" (
         echo Compilation !laterium_path_file!!file_extension!...: [yes]
