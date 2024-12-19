a563282b14f19154f25546f2d54cd4481baf1ae8
Socket <97043303+universestate@users.noreply.github.com>
2024-12-19 19:36:48 +0700

    Update batch.bat

--- source/batch.bat
+++ source/batch.bat
@@ -19,7 +19,7 @@ set "_SearchDir_=%~dp0"
 set time=%time%
 set time=%time: =0%
 
-title type help to get started
+title type `help` to get started
 
 for /f "tokens=1-3 delims=:." %%a in ("%time%") do set newtime=%%a%%b.%%c
 
@@ -44,11 +44,11 @@ echo Compiling...
     findstr /i "error" rus.txt > nul
     echo Errorlevel is: %errorlevel%
     if %errorlevel% equ 0 (
-        echo Error Status...: [yes]
-        goto end
-    ) else (
         echo Error Status...: [no]
         goto _start_this
+    ) else (
+        echo Error Status...: [yes]
+        goto end
     )
 
 :_start_this
@@ -131,12 +131,13 @@ goto end
     goto end
 
 ) else if "%typeof%"=="help" (
-:_help
-call :_hash_
 
-echo usage: cat [-c compile] [-r running server] [-ci compile-running] [-cls clear screen]
-echo       [-v laterium version] [-vsc vscode tasks]
-goto cmd
+    :_help
+    call :_hash_
+
+    echo usage: cat [-c compile] [-r running server] [-ci compile-running] [-cls clear screen]
+    echo       [-v laterium version] [-vsc vscode tasks]
+    goto cmd
 
 ) else if "%typeof%"=="cat" (
 
@@ -202,19 +203,21 @@ goto :eof
     echo Starting compilation..
     echo.
 
-    "!laterium_pawncc_path!" "!found_file!" -o"!found_file!.amx" -d0 > rus.txt 2>&1
+    set "output_file=!found_file:~0,-4!.amx"
+
+    "!laterium_pawncc_path!" "!found_file!" -o"!output_file!" -d0 > rus.txt 2>&1
 
     type rus.txt
 
-    if exist "!found_file!.amx" (
-        echo Compilation !found_file!...: [yes]
+    if exist "!output_file!" (
+        echo Compilation !output_file!...: [yes]
         echo.
 
-        for %%A in ("!found_file!.amx") do (
-            echo Total Size !found_file!.amx / %%~zA bytes
+        for %%A in ("!output_file!") do (
+            echo Total Size !output_file! / %%~zA bytes
         )
     ) else (
-        echo Compilation !found_file!...: [no]
+        echo Compilation !output_file!...: [no]
     )
 
     echo.
@@ -223,5 +226,5 @@ goto :eof
 
 :_hash_
     set "compn=%username%@%computername%"
-    for /f "delims=" %%H in ('powershell -command "[System.BitConverter]::ToString((New-Object System.Security.Cryptography.SHA1Managed).ComputeHash([System.Text.Encoding]::UTF8.GetBytes('%compn%'))).Replace('-','').ToLower()"') do set "hash=%%H"
+    for /f "delims=" %%H in ('powershell -NoProfile -Command "[System.BitConverter]::ToString((New-Object System.Security.Cryptography.SHA1Managed).ComputeHash([System.Text.Encoding]::UTF8.GetBytes('%compn%'))).Replace('-','').ToLower()"') do set "hash=%%H"
     title %compn% ^| %hash%
